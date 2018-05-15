3b86b05a8c1c921d81d6dd4d1c4202515010bf55
Julian Gan <juliangan07@gmail.com>
2018-05-14 22:16:48 -0400

    express

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use('/api/health-check', (req, res) => res.json({up: true}));
+
+// routes
+app.use('/', require('./routes'));
+// app.use('/api', require('./routes/api'));
+
+app.get('/', (req, res) => {
+    console.log(res.params);
+    res.send('Hello World');
+});
+
+// redirect to '/' if no route found
+app.use((req, res, next) => {
+    console.log('Page not found, redirecting to index.');
+    res.redirect('/');
+  });
+
+  module.exports = app;
