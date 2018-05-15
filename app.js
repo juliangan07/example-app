e51af21bfe0d14bf979c4230efa369a35aa1b520
Julian Gan <juliangan07@gmail.com>
2018-05-14 22:54:45 -0400

    more comments and set port

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 const express = require('express')
 const app = express()
 
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.set('port', (process.env.PORT || 3000));
+const port = app.get('port');
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use('/api/health-check', (req, res) => res.json({up: true}));
 
 // routes
-app.use('/', require('./routes'));
+// app.use('/', require('./routes'));
 // app.use('/api', require('./routes/api'));
 
 app.get('/', (req, res) => {
@@ -16,9 +19,9 @@ app.get('/', (req, res) => {
 });
 
 // redirect to '/' if no route found
-app.use((req, res, next) => {
-    // console.log('Page not found, redirecting to index.');
-    res.redirect('/');
-  });
+// app.use((req, res, next) => {
+//     // console.log('Page not found, redirecting to index.');
+//     res.redirect('/');
+//   });
 
   module.exports = app;
