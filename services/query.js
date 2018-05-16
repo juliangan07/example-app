d750fff7acf80b3489522ff4e38a6441fb9d60e7
Julian Gan <juliangan07@gmail.com>
2018-05-15 22:00:50 -0400

    added logic for query

--- services/query.js
+++ services/query.js
@@ -14,8 +14,52 @@ class QueryService {
                 return 'OK';
                 break;
 
+            case 'Phone':
+                return '+1 (405) 888 - 1353';
+                break;
+
+            case 'Email Address':
+                return 'juliangan07@gmail.com';
+                break;
+
+            case 'Source':
+                return 'https://github.com/juliangan07/example-app';
+                break;
+
+            case 'Resume':
+                return 'https://www.dropbox.com/s/1859x8vt3oqvb91/Cover%20Letter%20-%20EMX.pdf?dl=0';
+                break;
+
+            case 'Puzzle':
+                return '';
+                break;
+
+            case 'Years':
+                return '1.5 Years';
+                break;
+
+            case 'Name':
+                return 'Julian Shang-Zhi Gan';
+                break;
+
+            case 'Referrer':
+                return 'Jenny Gasparis';
+                break;
+
+            case 'Status':
+                return 'Yes';
+                break;
+
+            case 'Degree':
+                return 'Bachelors of Business & Administration, majoring in Information Systems';
+                break;
+
+            case 'Position':
+                return 'Full Stack Software Engineer';
+                break;
+
             default:
-                return 'TESTING';
+                return false;
                 break;
         }
     }
